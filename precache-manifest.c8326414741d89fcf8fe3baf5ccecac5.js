6967adf368b0fa1a9793e96f97042d430c02e6b1
Steve Thompson <sthmpsn@gmail.com>
2019-05-01 10:56:33 -0500

    Updates

---
+++ precache-manifest.c8326414741d89fcf8fe3baf5ccecac5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9c00a27f0dff48690447c45382844b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9c51e344c2d8225615c4",
+    "url": "/clicky-game/static/css/main.e8646917.chunk.css"
+  },
+  {
+    "revision": "214ee3e0b25f309e1a47",
+    "url": "/clicky-game/static/js/2.2d0aaf20.chunk.js"
+  },
+  {
+    "revision": "9c51e344c2d8225615c4",
+    "url": "/clicky-game/static/js/main.3cfd7944.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "48d36fbfec5c14160165bc9f75f4af05",
+    "url": "/clicky-game/static/media/Simpsonfont.48d36fbf.ttf"
+  },
+  {
+    "revision": "d6f83ca320a96dd950ad1c729bea5f3a",
+    "url": "/clicky-game/static/media/Simpsons_cast.d6f83ca3.png"
+  },
+  {
+    "revision": "2c1b7a58b1d8dbe4645e7dd80fb333ea",
+    "url": "/clicky-game/static/media/brand1.2c1b7a58.png"
+  },
+  {
+    "revision": "4678d644cb681ceac3a0c1f097ddf1a8",
+    "url": "/clicky-game/static/media/personal-use.4678d644.gif"
+  }
+]);
